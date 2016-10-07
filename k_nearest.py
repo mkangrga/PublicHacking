535deb54469b98dd476070e64f8b40916fb0be72
mkangrga <mkangrga@gmail.com>
2016-10-07 17:40:56 -0400

    Null.

--- k_nearest.py
+++ k_nearest.py
@@ -1,26 +1,41 @@
 import numpy as np
 from sklearn import preprocessing, cross_validation, neighbors
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+from collections import Counter
+style.use('fivethirtyeight')
 
-df = pd.read_csv('breast-cancer-wisconsin.data')
-df.replace('?', -99999, inplace=True)
-df.drop(['id'], 1, inplace=True)
+dataset = {'k' : [[1,2], [2,3], [3,1]], 'r': [[6,5], [7,7], [8,6]]}
+new_features = [5,7]
 
-X = np.array(df.drop(['class'], 1))
-y = np.array(df['class'])
+# [[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
+# plt.scatter(new_features[0], new_features[1])
+# plt.show()
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
-clf = neighbors.KNeighborsClassifier()
-clf.fit(X_train, y_train)
 
-accuracy = clf.score(X_test, y_test)
 
-print(accuracy)
+def old_code():
+    df = pd.read_csv('breast-cancer-wisconsin.data')
+    df.replace('?', -99999, inplace=True)
+    df.drop(['id'], 1, inplace=True)
 
-example_measures = np.array([[10,10,1,1,1,2,3,2,1],[4,2,1,2,2,2,3,2,1]])
-example_measures = example_measures.reshape(len(example_measures), -1)
+    X = np.array(df.drop(['class'], 1))
+    y = np.array(df['class'])
 
-prediction = clf.predict(example_measures)
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
-print(prediction)
+    clf = neighbors.KNeighborsClassifier()
+    clf.fit(X_train, y_train)
+
+    accuracy = clf.score(X_test, y_test)
+
+    print(accuracy)
+
+    example_measures = np.array([[10,10,1,1,1,2,3,2,1],[4,2,1,2,2,2,3,2,1]])
+    example_measures = example_measures.reshape(len(example_measures), -1)
+
+    prediction = clf.predict(example_measures)
+
+    print(prediction)
