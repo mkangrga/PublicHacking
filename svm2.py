c6b2400244836f4c7e41c34834cf4245d8f88d22
mkangrga <mkangrga@gmail.com>
2016-10-10 18:06:31 -0400

    Null.

--- svm2.py
+++ svm2.py
@@ -17,7 +17,7 @@ style.use('ggplot')
 data_dict = {-1: np.array([[1, 7],
                            [2, 8],
                            [3, 8],]),
-            1: np.array([[5, 1],
+             1: np.array([[5, 1],
                          [6, -1],
                          [7, 3]])}
 
