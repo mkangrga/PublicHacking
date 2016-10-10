faed534c61a77e6a638d0db16a81340391555f2f
mkangrga <mkangrga@gmail.com>
2016-10-10 18:06:03 -0400

    Null.

---
+++ svm2.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+style.use('ggplot')
+
+
+
+
+
+
+
+
+
+
+
+
+data_dict = {-1: np.array([[1, 7],
+                           [2, 8],
+                           [3, 8],]),
+            1: np.array([[5, 1],
+                         [6, -1],
+                         [7, 3]])}
+
