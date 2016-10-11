0fe3e8ca4937e537ecb7e9c727ea0b758f012cfc
mkangrga <mkangrga@gmail.com>
2016-10-11 18:12:29 -0400

    Null.

--- svm2.py
+++ svm2.py
@@ -19,7 +19,7 @@ class Support_Vector_Machine:
         opt_dict = {}
 
         transforms = [[1, 1],
-                      [ -1, 1],
+                      [-1, 1],
                       [-1, -1],
                       [1, -1]]
 
@@ -44,10 +44,10 @@ class Support_Vector_Machine:
         # with b as we do w
         b_multiple = 5
 
-        latest_optiumum = self.max_feature_value*10
+        latest_optimum = self.max_feature_value*10
 
         for step in step_size:
-            w = np.array([latest_optiumum, latest_optiumum])
+            w = np.array([latest_optimum, latest_optimum])
 
             # we can do this because convex
             optimized = False
@@ -62,10 +62,12 @@ class Support_Vector_Machine:
                         # SMO attempts to fix this a bit
                         # yi (xi.w + b) >= 1
                         for i in self.data:
+                            if not found_option: break
                             for xi in self.data[i]:
                                 yi = i
                                 if not yi * (np.dot(w_t, xi) + b) >= 1:
                                     found_option = False
+                                    break
                                     # add break here later
 
                         if found_option:
@@ -81,15 +83,50 @@ class Support_Vector_Machine:
             opt_choice = opt_dict[norms[0]]
             self.w = opt_choice[0]
             self.b = opt_choice[1]
-            latest_optiumum = opt_choice[0][0]
+            latest_optimum = opt_choice[0][0]
 
 
     def predict(self, features):
         # sign(x.w + b)
         classification = np.sign(np.dot(np.array(features), self.w) + self.b)
-
+        if classification != 0 and self.visualization:
+            self.ax.scatter(features[0], features[1], s = 200, marker='*', c=self.colors[classification])
         return classification
 
+    def visualize(self):
+        [[self.ax.scatter(x[0], x[1], color = self.colors[i]) for x in data_dict[i]] for i in data_dict]
+
+        # hyperplane = x.w+b
+        # v = x.w+b
+        # psv = 1
+        # nsv = -1
+        # dec = 0
+        def hyperplane(x, w, b, v):
+            return (-w[0]*x-b+v) / w[1]
+
+        datarange = (self.min_feature_value * 0.9, self.max_feature_value * 1.1)
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
+
+        # (w.x+b) = 1
+        # positive support vector hyperplane
+        psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
+        psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
+        self.ax.plot([hyp_x_min, hyp_x_max], [psv1, psv2])
+
+        # (w.x+b) = 1
+        # negative support vector hyperplane
+        nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)
+        nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
+        self.ax.plot([hyp_x_min, hyp_x_max], [nsv1, nsv2])
+
+        # (w.x+b) = 1
+        # decision boundary
+        db1 = hyperplane(hyp_x_min, self.w, self.b, 0)
+        db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
+        self.ax.plot([hyp_x_min, hyp_x_max], [db1, db2])
+
+        plt.show()
 
 data_dict = {-1: np.array([[1, 7],
                            [2, 8],
@@ -98,3 +135,20 @@ data_dict = {-1: np.array([[1, 7],
                           [6, -1],
                           [7, 3]])}
 
+
+svm = Support_Vector_Machine()
+svm.fit(data_dict)
+
+predict_us = [[0,10],
+              [1,3],
+              [3,4],
+              [3,5],
+              [5,5],
+              [5,6],
+              [6,-5],
+              [5,8],]
+
+for p in predict_us:
+    svm.predict(p)
+
+svm.visualize()
