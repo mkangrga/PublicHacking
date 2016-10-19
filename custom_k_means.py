62d422e28436c156e5bea0f7922004bbf439d192
mkangrga <mkangrga@gmail.com>
2016-10-19 18:15:01 -0400

    Null.

--- custom_k_means.py
+++ custom_k_means.py
@@ -11,21 +11,43 @@ X = np.array([[1, 2],
              [1, 0.6],
              [9, 11]],)
 
-# plt.scatter(X[:, 0], X[:, 1], s=150)
-# plt.show()
-
-clf = KMeans(n_clusters=2)
-clf.fit(X)
-
-centroids = clf.cluster_centers_
-labels = clf.labels_
-
-colors = ["g.", "r.", "c.", "b.", "k.", "o."]
-
-for i in range(len(X)):
-    plt.plot(X[i][0], X[i][1], colors[labels[i]], markersize=25)
-
-plt.scatter(centroids[:,0], centroids[:,1], marker="x", s=150, linewidths=5)
+plt.scatter(X[:, 0], X[:, 1], s=150)
 plt.show()
-print(clf.predict([20, 20]))
 
+colors = ["g", "r", "c", "b", "k", "o"]
+
+class K_means:
+    def __init__(self, k=2, tol=0.001, max_iter=300):
+        self.k = k
+        self.tol = tol
+        self.max_iter = max_iter
+
+    def fit(self, data):
+        self.centroids = {}
+
+        for i in range(self.k):
+            self.centroids[i] = data[i]
+
+        for i in range(self.max_iter):
+            self.classifications = {}
+
+            for i in range(self.k):
+                self.classifications[i] = []
+
+            for featureset in data:
+                distances = [np.linalg.norm(featureset-self.centroids[centroid]) for centroid in self.centroids]
+                classification = distances.index(min(distances))
+                self.classifications[classification].append(featureset)
+
+            prev_centroids = dict(self.centroids)
+
+            for classification in self.classifications:
+                pass
+                # self.centroids[classification] = np.average(self.classifications[classification], axis=0)
+
+
+    def predict(self, data):
+        pass
+
+clf = K_means(k=2)
+clf.fit(X)
